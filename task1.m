528ae265fce7bbfeb4a7d4b6192947922cbb9150
Antonio RICCIARDI <aricciar@primura-linux.cs.ucl.ac.uk>
2017-01-13 15:59:14 +0000

    added files

---
+++ task1.m
@@ -0,0 +1,12 @@
+load('data');
+dwis=double(dwis);
+dwis=permute(dwis,[4,1,2,3]);
+
+% Middle slice of the first image volume, which has b=0
+imshow(flipud(squeeze(dwis(1,:,:,72))'), []);
+% Middle slice of the second image volume with b=1000
+imshow(flipud(squeeze(dwis(2,:,:,72))'), []);
+
+qhat = load( 'bvecs' );
+and the b-value array, which we can construct like this:
+bvals = 1000*sum(qhat.*qhat);
