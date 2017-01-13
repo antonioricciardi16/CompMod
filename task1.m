1fd13f075b9f9c4fe3fad0a32488e2d5f785d90c
Antonio RICCIARDI <aricciar@primura-linux.cs.ucl.ac.uk>
2017-01-13 16:03:30 +0000

    small comment

--- task1.m
+++ task1.m
@@ -7,6 +7,8 @@ imshow(flipud(squeeze(dwis(1,:,:,72))'), []);
 % Middle slice of the second image volume with b=1000
 imshow(flipud(squeeze(dwis(2,:,:,72))'), []);
 
+% new comment
+
 qhat = load( 'bvecs' );
 and the b-value array, which we can construct like this:
 bvals = 1000*sum(qhat.*qhat);
